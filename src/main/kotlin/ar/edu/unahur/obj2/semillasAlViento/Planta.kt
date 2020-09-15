f9cab2cbaecbfc9e079789e33f70ac3065a03cb1
AngeliM96 <matiasangeli@gmail.com>
2020-09-15 19:17:33 -0300

    Terminado el ejercicio

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
@@ -1,13 +1,14 @@
 package ar.edu.unahur.obj2.semillasAlViento
 
-abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {
+abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) { // Mutación controlada: Altura debería ser val siendo que el objetivo es que no cambie nunca.
   fun esFuerte() = this.horasDeSolQueTolera() > 10
 
   fun parcelaTieneComplicaciones(parcela: Parcela) =
-    parcela.plantas.any { it.horasDeSolQueTolera() < parcela.horasSolPorDia }
+    parcela.plantas.any { it.horasDeSolQueTolera() < parcela.horasSolPorDia } // Cohesión: Este metodo debería ser de la parcela, no de la planta.
 
   abstract fun horasDeSolQueTolera(): Int
-  abstract fun daSemillas(): Boolean
+  // Redundancia minima: daSemillas podría ser una open fun que devuelva this.esFuerte() y las funciones que le hacen override llamar a la super() en vez de repetir tantas veces this.esFuerte()
+  abstract fun daSemillas(): Boolean  
 }
 
 class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemilla, altura) {
@@ -16,6 +17,7 @@ class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemi
 }
 
 class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean) : Planta(anioObtencionSemilla, altura) {
+  // (Des)acoplamiento y cohesión: Soja Transgénica debería ser una nueva clase que herede soja, que esté todo acoplado en una sola clase genera problemas de cohesión en la misma.
   override fun horasDeSolQueTolera(): Int  {
     // ¡Magia de Kotlin! El `when` es como un `if` pero más poderoso:
     // evalúa cada línea en orden y devuelve lo que está después de la flecha.
